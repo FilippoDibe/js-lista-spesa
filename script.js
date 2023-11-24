d8212d9110397b973603f61fcc456da0603df133
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-11-24 16:21:24 +0100

    fix refreshLista e aggiungiItem

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
-// creazione array 
+// Array di partenza con elementi predefiniti
 var listaSpesa = ["pane", "pasta", "acqua"];
 
-// riferimento di stampa 
+// Riferimento all'elemento UL dove verrà stampata la lista
 var stampaLista = document.getElementById('lista-spesa');
-var i =0;
 
-// ciclo while 
-while (i < listaSpesa.length){
-    var elementLI=document.createElement('li');
-    elementLI.textContent= listaSpesa[i];
+// Inizializzazione dell'indice per il ciclo while
+var i = 0;
 
-    // aggiungo alla lista 
+// Ciclo while per stampare gli elementi della lista al caricamento della pagina
+while (i < listaSpesa.length) {
+    // Creazione di un nuovo elemento LI
+    var elementLI = document.createElement('li');
+    elementLI.textContent = listaSpesa[i];
     stampaLista.appendChild(elementLI);
-
-    i++
+    i++;
 }
-// aggiungo un elemento alla array 
-function aggiungiItem(){
-    var newItem=document.getElementById('text');
 
-    // verifca dell input che non deve essere vuoto 
-    if (newItem !==''){
+// Funzione che viene chiamata al click del bottone "Aggiungi"
+function aggiungiItem() {
+    // Ottenimento del valore dell'input
+    var newItem = document.getElementById('text').value;
+    // Verifica se l'input non è vuoto prima di aggiungere l'elemento alla lista
+    if (newItem.trim() !== '') {
+        // Aggiunta del nuovo elemento all'array
         listaSpesa.push(newItem);
-        aggiungiItem()
-
-        // pulisce il campo 
-        document.getElementById(text).value='';
+        // Chiamata alla funzione per aggiornare la lista
+        refreshLista();
+        // Pulizia del campo di input
+        document.getElementById('text').value = '';
+    } else {
+        // Avviso se l'utente cerca di aggiungere un elemento vuoto
+        alert('Inserisci un elemento valido');
     }
 }
+
+// Funzione per aggiornare la lista
+function refreshLista() {
+    var listaAggiornata = document.getElementById('lista-spesa');
+    
+    listaAggiornata.innerHTML = '';
+
+    // Inizializzazione dell'indice per il ciclo while
+    var k = 0;
+
+    // Ciclo while per aggiornare la lista con gli elementi dell'array
+    while (k < listaSpesa.length) {
+        // Creazione di un nuovo elemento LI
+        var listItem = document.createElement('li');
+        listItem.textContent = listaSpesa[k];
+        listaAggiornata.appendChild(listItem);
+
+        k++;
+    }
+}
+
+
 
 
 
