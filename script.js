9e33811117590347f64c7c643cf7bf50d89b99b4
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-11-24 15:23:29 +0100

    add function start

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 var listaSpesa = ["pane", "pasta", "acqua"];
 
 // inizializzo e visualizzo la lista
+function start(){
+    var item = document.getElementById("lista-spesa");
+    var i = 0;
+
+   
+}
 
 
 
