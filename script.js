cd1277b0c07395242b4cc09f76e25d970ca69683
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-11-24 15:39:25 +0100

    fix ciclo while

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 // creazione array 
 var listaSpesa = ["pane", "pasta", "acqua"];
 
-// inizializzo e visualizzo la lista
-function start(){
-    var item = document.getElementById("lista-spesa");
-    var i = 0;
-    while (i < listaSpesa.length){
-        var listaItem= document.createElement('li');
-        listaItem.appendChild(document.createTextNode(listaSpesa[i]));
-        item.appendChild(listaItem);
-    }
-   
+// riferimento di stampa 
+var stampaLista = document.getElementById('lista-spesa');
+var i =0;
+
+// ciclo while 
+while (i < listaSpesa.length){
+    var elementLI=document.createElement('li');
+    elementLI.textContent= listaSpesa[i];
+
+    // aggiungo alla lista 
+    stampaLista.appendChild(elementLI);
+
+    i++
 }
+
 
 
 
